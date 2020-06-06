7f2ac69e26607cc7bd1c0a6dab909361a1a9d8c6
phillmac <phillmackintosh@gmail.com>
2020-06-06 05:12:12 +0000

    Remove spurious go-ipfs deps

--- api-ipfs.js
+++ api-ipfs.js
@@ -2,6 +2,7 @@ const IpfsHttpClient = require('ipfs-http-client')
 
 async function run () {
   const ipfsHost = process.env.IPFS_HOST | 'http://ipfs:5001'
+  console.info({ipfsHost})
   const ipfs = await IpfsHttpClient(ipfsHost)
 
   const resolve = require('./resolve.js')
