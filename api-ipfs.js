d75914606f6046c3b38194a83d6239ab8b2a1e73
phillmac <phillmackintosh@gmail.com>
2020-06-06 05:14:03 +0000

    Fix typo

--- api-ipfs.js
+++ api-ipfs.js
@@ -1,7 +1,7 @@
 const IpfsHttpClient = require('ipfs-http-client')
 
 async function run () {
-  const ipfsHost = process.env.IPFS_HOST | 'http://ipfs:5001'
+  const ipfsHost = process.env.IPFS_HOST || 'http://ipfs:5001'
   console.info({ipfsHost})
   const ipfs = await IpfsHttpClient(ipfsHost)
 
